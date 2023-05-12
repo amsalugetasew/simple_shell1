c14787f952a57f189c94154957c09916d9434d88
amsalugetasew <amsalugetasew12@gmail.com>
2023-05-12 05:17:49 -0700

    Commit read text file to the end

--- readtext_with_out_getline.c
+++ readtext_with_out_getline.c
@@ -8,14 +8,21 @@
  */
 int main(void)
 {
-	size_t size = 10;
+	size_t size = 0;
 	char *str;
 
 	printf("please enter string: ");
 	str = (char *)malloc(size);
 	scanf("%[^\n]s", str);
 	size = strlen(str) + 1;
-	printf("The input string is: %s\n", str);
-	printf("The memory block size of current string is: %ld\n", size);
+	if (size == 1)
+	{
+		printf("ERROR!\n");
+	}
+	else
+	{
+		printf("The input string is: %s\n", str);
+		printf("The memory block size of current string is: %ld\n", size);
+	}
 	return (0);
 }
